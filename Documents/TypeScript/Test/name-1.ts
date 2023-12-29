a2ab796e9564ed20b6cc0c194754d0f3a940fb2c
EnverJ <Enlem1987@gmail.com>
2023-12-28 21:23:12 -0700

    ts

---
+++ Documents/TypeScript/Test/name-1.ts
@@ -0,0 +1,6 @@
+console.log("Hello Typescript");
+ // .ts --> tsc(TS compiler) --> .js --> JavaScript RuntTime Env: Browser, NodeJS, App
+
+ let age: number=30;
+ let fistName:string="Enver"
+ let isActive: boolean =true
