31e5b9ee82b13013ff95ea471d873993b9daaeb9
EnverJ <Enlem1987@gmail.com>
2023-12-28 21:29:04 -0700

    ts

---
+++ lang-1.js
@@ -0,0 +1,14 @@
+// function getInf() {
+//   if (firstName.length > 10) {
+//     console.log("PASS");
+//   } else {
+//     console.log("FAIL");
+//   }
+// }
+// getInf();
+
+const user = {
+  fistName: "tom",
+  age: 20,
+};
+console.log(user.city);
---
+++ name-1.js
@@ -0,0 +1,5 @@
+console.log("Hello Typescript");
+// .ts --> tsc(TS compiler) --> .js --> JavaScript RuntTime Env: Browser, NodeJS, App
+var age = 30;
+var fistName = "Enver";
+var isActive = true;
