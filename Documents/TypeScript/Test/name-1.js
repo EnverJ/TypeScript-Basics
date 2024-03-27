16060ec75f96cc83eb488929ef968535812b48c6
EnverJ <Enlem1987@gmail.com>
2024-03-27 16:10:08 -0600

    some changes

---
+++ Documents/TypeScript/Test/name-1.js
@@ -0,0 +1,15 @@
+console.log("Hello Typescript");
+// .ts --> tsc(TS compiler) --> .js --> JavaScript RuntTime Env: Browser, NodeJS, App
+var age = 30;
+var fistName = "Enver";
+//  let isActive: boolean = true;
+var lastName = "EE";
+console.log(lastName);
+var price = 23;
+console.log(price);
+// more hands on
+var myAge = 50;
+var school = "UC";
+var speed = 100;
+var speeding = true;
+console.log(myAge + " " + school + " " + speed + " " + speeding);
