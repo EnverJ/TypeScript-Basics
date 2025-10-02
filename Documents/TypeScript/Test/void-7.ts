be518764d8850b374079eb59ac9a615f89b1ee33
EnverJ <Enlem1987@gmail.com>
2025-10-01 21:29:44 -0600

    remove delete files

--- Documents/TypeScript/Test/void-7.ts
+++
@@ -1,7 +0,0 @@
-let num2: void; // void = undefined; this is only possible value
-
-function getInfo(msg: string): void {
-  console.log(msg);
-}
-
-getInfo("hi ts!!!");
--- VS/course-api-data/course-api-data/src/main/java/io/ezoapi/courseapidata/topic/TopicRepository.java
+++
@@ -1,5 +0,0 @@
-package io.ezoapi.courseapidata.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {}
